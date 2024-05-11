3f155c502c829e6f294b64db612a3700fc6c7051
aziz <aziz.adel.fci@gmail.com>
2024-05-11 18:01:13 +0300

    fix : remove home page cache

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -22,40 +22,40 @@ class HomeController extends Controller
 {
     public function index(Request $request)
     {
-        if (!Cache::has('featured_ads')) {
-            Cache::remember('featured_ads', 5 * 60 * 60, function () {
-                return Ads::with('media')->where('is_featured',1)->orderBy('home_order')->get();
-            });
-        }
-        if (!Cache::has('featured_services')) {
-            Cache::remember('featured_services', 5 * 60 * 60, function () {
-                return Service::with('media')->where('is_featured',1)->orderBy('home_order')->get();
-            });
-        }
-        if (!Cache::has('employees')) {
-            Cache::remember('employees', 5 * 60 * 60, function () {
-                return Employee::with('media')->orderBy('created_at')->get();
-            });
-        }
-        if (!Cache::has('services')) {
-            Cache::remember('services', 5 * 60 * 60, function () {
-                return Service::with('media')->take(10)->get();
-            });
-        }
-        if (!Cache::has('courses')) {
-            Cache::remember('courses', 5 * 60 * 60, function () {
-                return Course::with(['lecturer','topic','media'])->orderByDesc('date')->take(5)->get();
-            });
-        }
+//        if (!Cache::has('featured_ads')) {
+//            Cache::remember('featured_ads', 5 * 60 * 60, function () {
+//                return Ads::with('media')->where('is_featured',1)->orderBy('home_order')->get();
+//            });
+//        }
+//        if (!Cache::has('featured_services')) {
+//            Cache::remember('featured_services', 5 * 60 * 60, function () {
+//                return Service::with('media')->where('is_featured',1)->orderBy('home_order')->get();
+//            });
+//        }
+//        if (!Cache::has('employees')) {
+//            Cache::remember('employees', 5 * 60 * 60, function () {
+//                return Employee::with('media')->orderBy('created_at')->get();
+//            });
+//        }
+//        if (!Cache::has('services')) {
+//            Cache::remember('services', 5 * 60 * 60, function () {
+//                return Service::with('media')->take(10)->get();
+//            });
+//        }
+//        if (!Cache::has('courses')) {
+//            Cache::remember('courses', 5 * 60 * 60, function () {
+//                return Course::with(['lecturer','topic','media'])->orderByDesc('date')->take(5)->get();
+//            });
+//        }
         return view('welcome',[
             'book'=> Book::with('media')->where('type_id','1')->IsActive()->latest()->first(),
             'articles'=> Article::with(['writer','media'])->orderByDesc('date')->IsActive()->take(2)->get(),
-            'courses'=> Cache::get('courses'),
-            'employees' => Cache::get('employees'),
-            'services'=> Cache::get('services'),
+            'courses'=> Course::with(['lecturer','topic','media'])->orderByDesc('date')->take(5)->get(),
+            'employees' => Employee::with('media')->orderBy('created_at')->get(),
+            'services'=> Service::with('media')->take(10)->get(),
             'home'=> HomeConfigs::first(),
-            'featured_ads' => Cache::get('featured_ads'),
-            'featured_services' => Cache::get('featured_services'),
+            'featured_ads' => Ads::with('media')->where('is_featured',1)->orderBy('home_order')->get(),
+            'featured_services' => Service::with('media')->where('is_featured',1)->orderBy('home_order')->get(),
             'youtube' => ContactData::where('title', 'youtube')->first()['value'],
             'locale' => request()->session()->get('locale'),
         ]);
